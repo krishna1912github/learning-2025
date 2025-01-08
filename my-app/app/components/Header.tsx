0f2d54c5df229fc32da9c0684478f6ca98efc7de
krishna1912github <krishnakumar.s1912@gmail.com>
2025-01-08 20:53:44 +0530

    added logo

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import Logo from '../asserts/Logo.png'
+import Image from 'next/image'
+import menu from '../asserts/menu.png'
+
 
 function Header() {
   return (
-<div className="mt-4 grid grid-cols-2 lg:grid-cols-3">
-       <div className="bg-yellow-100 grid grid-cols-2">
-        <div>icon</div>
-        <div>Personal</div>
-     </div>
-     <div className="bg-blue-100">Navgition</div>
-  </div>
+    <div className="mt-4 grid grid-cols-2 lg:grid-cols-3  px-6">
+      <div className=" grid grid-cols-2">
+        <div>
+          <Image
+            src={Logo}
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+        </div>
+      </div>
+      <div className="bg-blue-100  grid-cols-4 hidden  sm:block "></div>
+      <div className="grid justify-end">        
+        <div>
+        <Image
+            src={menu}
+            width={26}
+            height={26}
+            alt="Picture of the author"
+          />
+        </div>
+        </div>
+    </div>
   )
 }
 
