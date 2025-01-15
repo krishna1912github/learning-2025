ddbc53782b6b61e8ee61550d8f9d82ca1f7fd5f5
krishna1912github <krishnakumar.s1912@gmail.com>
2025-01-15 15:54:14 +0530

    add intro

---
+++ my-app/app/components/introduction.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const introduction = () => {
+  return (
+<div className="flex flex-col" >
+  <div className="">
+    <p className=''> Hello <span className='font-bold' >I’am Krishna.</span></p>
+    <p> <span className='font-bold' > Frontend  </span> Developer</p>
+    <p> Based In <span className='font-bold' >India</span></p>
+    </div>
+  <div className="bg-yellow-100">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.   </div>
+  <div className="bg-red-100">Block-3</div>
+</div>
+  )
+}
+
+export default introduction
