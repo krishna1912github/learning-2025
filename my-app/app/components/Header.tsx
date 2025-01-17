2ae23d58fd760d375688d9a69e60ec9321e43ea9
krishna1912github <krishnakumar.s1912@gmail.com>
2025-01-17 21:44:05 +0530

    added banner

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,35 +1,42 @@
 import React from 'react'
-import Logo from '../asserts/Logo.png'
-import Image from 'next/image'
-import menu from '../asserts/menu.png'
 
-
+import { IoMdMenu } from "react-icons/io";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { DiGithubBadge } from "react-icons/di";
 function Header() {
   return (
-    <div className="mt-4 grid grid-cols-2 lg:grid-cols-3  px-6">
-      <div className=" grid grid-cols-2">
-        <div>
-          <Image
-            src={Logo}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
+    <div className=" grid grid-cols-2 lg:grid-cols-3   px-6 lg:mt-10 lg:justify-center p-4 lg:px-14 ">
+      <div className="grid grid-cols-2">
+      <div>
+      <div className=" block lg:hidden">
+        <DiGithubBadge size={50} /> 
+      </div>
+      <div className="hidden lg:block ">
+        <DiGithubBadge size={40} />
+      </div>
+    </div>
+      </div>
+      <div className=" hidden sm:block">
+        <div className="flex items-stretch gap-5  py-2 justify-center">
+          <div className="text-lg font-semibold">About Me</div>
+          <div className="text-lg font-semibold">Skills</div>
+          <div className="text-lg font-semibold">Project</div>
+          <div className="text-lg font-semibold">Blogs</div>
+          <div className="text-lg font-semibold">Contact Me</div>
         </div>
       </div>
-      <div className="bg-blue-100  grid-cols-4 hidden  sm:block "></div>
-      <div className="grid justify-end">        
-        <div>
-        <Image
-            src={menu}
-            width={26}
-            height={26}
-            alt="Picture of the author"
-          />
+      <div className="grid justify-end">
+        <div className="lg:hidden">
+          <IoMdMenu size={50} />
         </div>
+        <div className="hidden lg:block ">
+          <button className="flex gap-2 justify-center p-2 bg-black text-white rounded">
+            Resume <MdOutlineFileDownload  size={20} /> 
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
--- my-app/app/components/introduction.tsx
+++ my-app/app/components/introduction.tsx
@@ -1,18 +1,37 @@
 import React from 'react'
-import {Fb} from './../asserts/iconSvg'
+import { Fb } from './../asserts/iconSvg'
+import Banner from './../asserts/Banner.png'
+import Image from 'next/image'
 const introduction = () => {
   return (
-<div className="flex flex-col px-8 gap-4" >
-  <div className="text-4xl tracking-wide">
-    <p className=''> Hello <span className='font-bold ' >I’am Krishna.</span></p>
-    <p> <span className='font-bold' > Fullstack  </span> Developer</p>
-    <p> Based In <span className='font-bold' >India</span></p>
+    <div className="flex flex-col px-8 gap-8 lg:px-16 lg:mt-10" >
+      <div className="text-3xl lg:text-5xl lg:mt-10 tracking-wide lg:flex flex-row ">
+        <div className='flex flex-col gap-8 lg:w-2/4 lg:mt-10'>       
+        <div className='flex flex-col lg:gap-5'>
+          <p className=''> Hello <span className='font-extrabold' >I’am Krishna.</span></p>
+          <p> <span className=' font-extrabold' > Fullstack  </span> Developer</p>
+          <p> Based In <span className='font-extrabold' >India</span></p>
+        </div>
+        <div className="text-xl text-zinc-500">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.   </div>
+      <div className="flex gap-4">
+        <Fb />
+        <Fb />
+        <Fb />
+        <Fb />
+        <Fb />
+      </div>
+      </div>
+      <div className='hidden sm:block lg:ml-8'>
+        <Image
+          src={Banner}
+          width={800}
+          height={800}
+          alt="Picture of the author"
+        />
+      </div>
+      </div>
+   
     </div>
-  <div className="text-xl text-zinc-500">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.   </div>
-  <div className="bg-red-100">
-    <Fb/>
-  </div>
-</div>
   )
 }
 
