04113d9684503a3881a01abbe2e6fc5cacfbfcd5
krishna <krishna.kumar@cloudactivelabs.com>
2025-01-20 20:01:43 +0530

    added skills card

--- my-app/app/components/skills.tsx
+++ my-app/app/components/skills.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import { DiJsBadge } from "react-icons/di";
+import { BiLogoTailwindCss } from "react-icons/bi";
+import { SiTypescript } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { FaNode } from "react-icons/fa6";
+import { BiLogoPostgresql } from "react-icons/bi";
 const skills = () => {
     return (
         <div className="mt-10 lg:mt-14 px-8 gap-8 flex flex-col" >
@@ -8,30 +13,70 @@ const skills = () => {
                     <p className='text-center'> My <span className='font-extrabold ' >Skills </span></p>
                 </div>
             </div>
-            <div className="bg-green-100">
+            <div className="">
                 <div className="mt-4 flex flex-col justify-center items-stretch lg:flex-row gap-4 ">
-                    <div className="bg-blue-100">
+                    <div className="">
                         <div className="flex lg:flex-col gap-4 ">
-                            <div className="bg-blue-100">Block-111</div>
-                            <div className="bg-yellow-100">Block-211</div>
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <DiJsBadge size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                    JavaScript
+                                </div>
+                            </div>
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <BiLogoTailwindCss size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                    Tailwind Css
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="bg-yellow-100">
-                        <div className="flex gap-4 lg:flex-col ">
-                            <div className="bg-blue-100">Block-111</div>
-                            <div className="bg-yellow-100">Block-211</div>
+                </div>
+                <div className="mt-4 flex flex-col justify-center items-stretch lg:flex-row gap-4 ">
+                    <div className="">
+                        <div className="flex lg:flex-col gap-4 ">
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <SiNextdotjs size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                    Next Js 
+                                </div>
+                            </div>
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <SiTypescript size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                Typescript
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="bg-red-100">
-                        <div className="flex gap-4 lg:flex-col">
-                            <div className="bg-blue-100">Block-111</div>
-                            <div className="bg-yellow-100">Block-211</div>
-                        </div>
-                    </div>
-                    <div className="bg-orange-100">
-                        <div className="flex gap-4 lg:flex-col ">
-                            <div className="bg-blue-100">Block-111</div>
-                            <div className="bg-yellow-100">Block-211</div>
+                </div>
+                <div className="mt-4 flex flex-col justify-center items-stretch lg:flex-row gap-4 ">
+                    <div className="">
+                        <div className="flex lg:flex-col gap-4 ">
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <FaNode size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                    Node js
+                                </div>
+                            </div>
+                            <div className=" flex flex-col   w-[160px] h-[160px] border-2 border-black ">
+                                <div className="flex justify-center py-6">
+                                    <BiLogoPostgresql size={58} />
+                                </div>
+                                <div className=" font-medium text-2xl flex justify-center animate__animated animate__bounceIn ">
+                                  Postgresql
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
