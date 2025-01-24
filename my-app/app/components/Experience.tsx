2c1278e54710d83d8594c86e53d1bd070b476e53
krishna1912github <krishnakumar.s1912@gmail.com>
2025-01-24 16:05:29 +0530

    add experience

--- my-app/app/components/Experience.tsx
+++ my-app/app/components/Experience.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { SiPrisma } from "react-icons/si";
 import logo from "./../asserts/cloudactive-labs-logo.svg"
-import aaldura from "./../asserts/ATS-Logo-Lite-qf51hv9hki3b7h1v1stbv12x8hva7kisapqdg7udgk.png"
+import aaldura from "./../asserts/all.jpeg"
 import freelancer from "./../asserts/pngwing.com.png"
 import { SiFreelancer } from "react-icons/si";
-
+import { FaAlgolia } from "react-icons/fa6";
 import Image from 'next/image'
 const Experience = () => {
     let iconStyles = { color: "skyblue" };
@@ -16,87 +16,101 @@ const Experience = () => {
                 </div>
             </div>
             <div>
-                <div className=" lg:px-11 flex flex-col gap-7" >
-                <div className="group hover:bg-slate-900 flex flex-col">
-                        <div className="flex flex-col gap-4 border-2 w-full  border-white rounded-lg p-6 ">
-                            <div className="flex flex-col gap-4">
-                                    <div className="flex gap-3" >
-                                        <div className="mt-[18px]">
-                                            <div>
-                                                <Image src={logo} alt="Experience Image" width={84} height={84} />
-                                            </div>
-                                        </div>
-                                        <div className=' '>
-                                            <div className='flex flex-col lg:gap-5'>
-                                                <p className='text-xl font-medium text-white'> Full Stack Engineer <span className='' >At Cloudactive labs </span> </p>
-                                                <p className=' block lg:hidden text-xl font-medium text-white  '> At Cloudactive labs </p>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                                <div className="text-xl font-normal text-white">May2022 - Present</div>
-                                <div className="text-lg text-justify font-light text-white">As a Senior Software Engineer at Google, I played a pivotal role
-                                     in developing innovative solutions for Google's core search algorithms. 
-                                     Collaborating with a dynamic team of engineers, I contributed to the 
-                                     enhancement of search accuracy and efficiency, optimizing user experiences 
-                                     for millions of users worldwide.
-                                     </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="group hover:bg-slate-900 flex flex-col ">
-                        <div className="flex flex-col gap-4 border-2 w-full  border-white rounded-lg p-6 ">
-                            <div className="flex flex-col gap-4">
-                                    <div className="flex gap-3" >
-                                        <div className="mt-[18px]">
-                                            <div>
-                                                <Image src={aaldura} alt="Experience Image" width={84} height={84} />
-                                            </div>
-                                        </div>
-                                        <div className="">
-                                            <div className='flex flex-col lg:gap-5 '>
-                                                <p className='text-xl font-medium text-white'> Jr.Full Stack Developer </p>
-                                                <p className='text-xl font-medium text-white'> At Aaludra Tech </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                <div className="text-xl font-normal text-white">May2022 - Present</div>
-                                <div className="text-lg font-light text-justify text-white">As a Senior Software Engineer at Google, I played a pivotal role
-                                     in developing innovative solutions for Google's core search algorithms. 
-                                     Collaborating with a dynamic team of engineers, I contributed to the 
-                                     enhancement of search accuracy and efficiency, optimizing user experiences 
-                                     for millions of users worldwide.
-                                     </div>
-                            </div>
-                        </div>
-                    </div>
-                    
+                <div className=" lg:px-11 flex flex-col gap-7 lg:gap-14" >
                     <div className="group hover:bg-slate-900 flex flex-col">
                         <div className="flex flex-col gap-4 border-2 w-full  border-white rounded-lg p-6 ">
                             <div className="flex flex-col gap-4">
-                                    <div className="flex gap-6" >
-                                        <div className="mt-[7px] justify-center">
-                                            <div>
-                                                <SiFreelancer  style={iconStyles} size={56}/> 
-                                            </div>
-                                        </div>
-                                        <div className="">
-                                            <div className='flex flex-col lg:gap-5'>
-                                                <p className='text-xl font-medium text-white'>Freelance Full Stack  </p>
-                                                <p className='text-xl font-medium text-white'>Developer & Consultant </p>
+                                <div className="flex gap-3 lg:justify-between">
+                                    <div className="lg:mt-[6px]">
+                                        <div className='flex flex-row gap-4'>
+                                            <Image src={logo} alt="Experience Image" width={84} height={84} />
+                                            <div className='flex-row lg:flex gap-2 lg:ml-[10px]'>
+                                                <p className='text-xl font-medium lg:text-2xl text-white'> Full Stack Engineer </p>
+                                                <p className='text-xl font-medium lg:text-2xl text-white  '>At Cloudactive Labs </p>
                                             </div>
                                         </div>
                                     </div>
-                                <div className="text-xl font-normal text-white">May2022 - Present</div>
-                                <div className="text-lg font-light text-justify text-white">As a Senior Software Engineer at Google, I played a pivotal role
-                                     in developing innovative solutions for Google's core search algorithms. 
-                                     Collaborating with a dynamic team of engineers, I contributed to the 
-                                     enhancement of search accuracy and efficiency, optimizing user experiences 
-                                     for millions of users worldwide.
-                                     </div>
+
+                                    <div className="hidden sm:block  lg:mt-[6px] text-xl font-normal text-white">May2022 - Present</div>
+                                </div>
+                                <div className="block lg:hidden text-xl font-normal text-white">May2022 - Present</div>
+                                <div className="text-lg  text-justify font-light text-white lg:text-xl ">
+                                    As a Senior Software Engineer at Google, I played a pivotal role
+                                    in developing innovative solutions for Google's core search algorithms.
+                                    Collaborating with a dynamic team of engineers, I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.
+                                    <span className="hidden sm:block">
+                                    I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="group hover:bg-slate-900 flex flex-col">
+                        <div className="flex flex-col gap-4 border-2 w-full  border-white rounded-lg p-6 ">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex gap-3 lg:justify-between">
+                                    <div className="lg:mt-[6px]">
+                                        <div className='flex flex-row gap-4'>
+                                        <Image src={aaldura} alt="Experience Image" width={53} height={50} />
+                                            <div className='flex-row lg:flex gap-2 lg:ml-[10px]'>
+                                                <p className='text-xl font-medium lg:text-2xl text-white'> Jr.Full Stack Developer </p>
+                                                <p className='text-xl font-medium lg:text-2xl text-white  '>At Aaludra Tech</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="hidden sm:block  lg:mt-[6px] text-xl font-normal text-white">May2022 - Present</div>
+                                </div>
+                                <div className="block lg:hidden text-xl font-normal text-white">May2022 - Present</div>
+                                <div className="text-lg  text-justify font-light text-white lg:text-xl ">
+                                    As a Senior Software Engineer at Google, I played a pivotal role
+                                    in developing innovative solutions for Google's core search algorithms.
+                                    Collaborating with a dynamic team of engineers, I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.
+                                    <span className="hidden sm:block">
+                                    I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="group hover:bg-slate-900 flex flex-col">
+                        <div className="flex flex-col gap-4 border-2 w-full  border-white rounded-lg p-6 ">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex gap-3 lg:justify-between">
+                                    <div className="lg:mt-[6px]">
+                                        <div className='flex flex-row gap-4'>
+                                        <SiFreelancer style={iconStyles} size={56} />
+                                            <div className='flex-row lg:flex gap-2 lg:ml-[10px]'>
+                                                <p className='text-xl font-medium lg:text-2xl text-white'>Freelance Full Stack </p>
+                                                <p className='text-xl font-medium lg:text-2xl text-white  '>Developer & Consultant </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="hidden sm:block  lg:mt-[6px] text-xl font-normal text-white">May2022 - Present</div>
+                                </div>
+                                <div className="block lg:hidden text-xl font-normal text-white">May2022 - Present</div>
+                                <div className="text-lg  text-justify font-light text-white lg:text-xl ">
+                                    As a Senior Software Engineer at Google, I played a pivotal role
+                                    in developing innovative solutions for Google's core search algorithms.
+                                    Collaborating with a dynamic team of engineers, I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.
+                                    <span className="hidden sm:block">
+                                    I contributed to the
+                                    enhancement of search accuracy and efficiency, optimizing user experiences
+                                    for millions of users worldwide.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 
                 </div>
             </div>
 
