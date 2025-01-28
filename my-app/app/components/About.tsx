e5bb889774975bace1a5c30dd823b9ea47828f77
krishna1912github <krishnakumar.s1912@gmail.com>
2025-01-28 20:48:58 +0530

    add projects

--- my-app/app/components/About.tsx
+++ my-app/app/components/About.tsx
@@ -4,38 +4,45 @@ import Image from 'next/image'
 export const About = () => {
     return (
         <div>
-            <div className='flex flex-col lg:flex-row lg:gap-24 justify-evenly p-8 lg:p-16'>
-                <div className='text-3xl lg:text-5xl  block lg:hidden'>
-                    <div>
-                        <p className='text-center py-8'> About <span className='font-extrabold ' >Me </span></p>
-                    </div>
-                </div>
-                <div className=' '>
-                    <Image src={me} alt="" width={1500} height={1500} />
-                </div>
-                <div className='flex flex-col gap-6 lg:gap-3 text-justify  text-base lg:text-lg'>
-                    <div className='text-3xl lg:text-5xl  hidden sm:block'>
-                        <div>
-                            <p className='text-center py-8'> About <span className='font-extrabold ' >Me </span></p>
-                        </div>
-                    </div>
-                    <div className='text-zinc-500 '>
-                        I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
-                        I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
-                        pixel perfect design, and writing clear, readable, highly performant code matters to me.
-                    </div>
-                    <div className='text-zinc-500'>
-                        I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
-                        I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
-                        pixel perfect design, and writing clear, readable, highly performant code matters to me.
-                    </div>
-                    <div className='text-zinc-500'>
-                        I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
-                        I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
-                        pixel perfect design, and writing clear, readable, highly performant code matters to me.
-                    </div>
-                </div>
+        <div className='flex flex-col gap-10 justify-evenly'>
+          <div className='text-3xl lg:text-5xl  '>
+            <div>
+              <p className='text-center py-8'> About <span className='font-extrabold ' >Me </span></p>
             </div>
+          </div>
+  
+          <div className='flex flex-col w-full lg:flex-row  justify-evenly px-8  '>
+  
+            <div className='lg:w-1/2'>
+              <Image
+                src={me}
+                alt="me"
+                className="w-full max-w-[630px] lg:h-[450px]"
+              />
+            </div>
+            <div className=' flex flex-col lg:w-1/2 gap-3 lg:gap-3 text-justify  text-base lg:text-lg max-w-[530px] '>
+              <div className=' text-zinc-500 '>
+              I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). 
+              I am very enthusiastic about bringing the technical and visual aspects of digital products to life. 
+              User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me.
+              </div>
+  
+              <div className=' text-zinc-500 '>
+              I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). 
+              I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me.
+              </div>
+              <div className=' text-zinc-500 '>
+                I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
+                I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
+              </div>
+
+            </div>
+          </div>
+  
+        
+  
+  
         </div>
+      </div>
     )
 }
--- my-app/app/components/Projects.tsx
+++ my-app/app/components/Projects.tsx
@@ -1,9 +1,103 @@
 import React from 'react'
-
+import me from './../asserts/image.png'
+import Image from 'next/image'
+import { CiShare1 } from "react-icons/ci";
 export const Projects = () => {
   return (
-    <div> projects
-  </div>
-    
+    <div>
+      <div className='bg-black flex flex-col gap-10 lg:gap-24 justify-evenly p-8 lg:p-17 mt-10'>
+        <div className='text-3xl lg:text-5xl  '>
+          <div>
+            <p className='text-center text-white py-8'> My <span className='font-extrabold ' >Projects </span></p>
+          </div>
+        </div>
+
+        <div className='flex flex-col lg:flex-row w-full justify-evenly items-center lg:px-10 '>
+
+          <div className='lg:w-1/2'>
+            <Image
+              src={me}
+              alt="me"
+              className="w-full max-w-[530px] lg:h-[397px]"
+            />
+          </div>
+          <div className=' flex flex-col lg:w-1/2 gap-3 lg:py-10 lg:gap-3 text-justify mt-5 text-base lg:text-lg max-w-[530px] lg:h-[397px]'>
+            <div className='text-white text-lg font-extrabold '>
+           01
+            </div>
+
+            <div className='text-white text-lg font-extrabold'>
+              I'm a passionate, self-proclaime.
+            </div>
+            <div className='text-zinc-500 font-bold'>
+              I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
+              I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
+              pixel perfect design, and writing clear, readable, highly performant code matters to me.
+            </div>
+            <div className='text-zinc-500'>
+              <CiShare1 size={30} className='text-white' />
+            </div>
+          </div>
+        </div>
+
+        <div className='flex flex-col lg:flex-row-reverse w-full justify-evenly items-center lg:px-10 '>
+          <div className='lg:w-1/2 flex justify-end'>
+            <Image
+              src={me}
+              alt="me"
+              className="w-full max-w-[530px] lg:h-[397px] "
+            />
+          </div>
+
+          <div className=' flex flex-col lg:w-1/2 gap-3 lg:py-10 lg:gap-3 text-justify mt-5 text-base lg:text-lg max-w-[530px] lg:h-[397px]'>
+            <div className='text-white text-lg font-extrabold'>
+              02
+            </div>
+
+            <div className='text-white text-lg font-extrabold'>
+              I'm a passionate, self-proclaime.
+            </div>
+            <div className='text-zinc-500 font-bold'>
+              I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
+              I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
+              pixel perfect design, and writing clear, readable, highly performant code matters to me.
+            </div>
+            <div className='text-zinc-500'>
+              <CiShare1 size={30} className='text-white' />
+            </div>
+          </div>
+        </div>
+
+        <div className='flex flex-col lg:flex-row w-full justify-evenly items-center lg:px-10 '>
+          <div className='lg:w-1/2'>
+            <Image
+              src={me}
+              alt="me"
+              className="w-full max-w-[530px] lg:h-[397px]"
+            />
+          </div>
+
+          <div className=' flex flex-col lg:w-1/2 gap-3 lg:py-10 lg:gap-3 text-justify mt-5 text-base lg:text-lg max-w-[530px] lg:h-[397px]'>
+            <div className='text-white text-lg font-extrabold'>
+              03
+            </div>
+
+            <div className='text-white text-lg font-extrabold'>
+              I'm a passionate, self-proclaime.
+            </div>
+            <div className='text-zinc-500 font-bold'>
+              I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js).
+              I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience,
+              pixel perfect design, and writing clear, readable, highly performant code matters to me.
+            </div>
+            <div className='text-zinc-500'>
+              <CiShare1 size={30} className='text-white' />
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   )
 }
